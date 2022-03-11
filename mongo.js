4b444601d1ee0a85700ee81ab531df818d88267c
Josh Yee <joshyee@gmail.com>
2022-03-10 18:00:22 -0800

    updates

--- mongo.js
+++ mongo.js
@@ -4,7 +4,7 @@ if (process.argv.length < 3) {
     console.log('Please provide the password as an argument: node mongo.js <password> <name> <number>')
     process.exit(1)
   }
-  
+
   const password = process.argv[2]
   const name = process.argv[3]
   const number = process.argv[4]
@@ -20,21 +20,25 @@ if (process.argv.length < 3) {
   })
   
   const Contact = mongoose.model('contact', contactSchema)
-  
-  const contact = new Contact({
-    name: name,
-    number: number,
-  })
 
-  
-  contact.save().then(result => {
-    console.log('The contact was saved!')
-    mongoose.connection.close()
-  })
+  if (process.argv.length === 3) {
+    Contact.find({}).then(result => {
+      console.log("Phonebook: ")
+      result.forEach(contact => {
+        console.log(contact.name, contact.number)
+      })
+      mongoose.connection.close()
+    })
+  } else {
+    const contact = new Contact({
+      name: name,
+      number: number,
+    })
 
-// Contact.find({}).then(result => {
-//     result.forEach(contact => {
-//       console.log(contact)
-//     })
-//     mongoose.connection.close()
-//   })
+    contact.save().then(result => {
+      console.log(`Added ${contact.name} Number: ${contact.number} to phonebook`)
+      mongoose.connection.close()
+    })
+  }
+
+
